2dbdf223cea3f3cb6b16dfb11448bed64b40197a
e253 <62357808+e253@users.noreply.github.com>
2020-11-18 22:12:17 -0500

    Another Broken

--- Test.py
+++ Test.py
@@ -8,18 +8,18 @@ webcam = cv2.VideoCapture(0)
 INPUT = webcam
 width = int(INPUT.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(INPUT.get(cv2.CAP_PROP_FRAME_HEIGHT))
-fps_in = INPUT.get(cv2.CAP_PROP_FPS)
-
+fps_in = int(INPUT.get(cv2.CAP_PROP_FPS))
+print(height, width, fps_in)
 
 with pyvirtualcam.Camera(width, height, fps_in, 0) as cam:
     while True:
-        status, frame = video.read()
-        print(frame.shape)
+        status, frame = INPUT.read()
+        print(frame)
         out_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        print(out_frame.shape)
+        print(out_frame)
         out_frame_rgba = np.zeros((height, width, 4), np.uint8)
         out_frame_rgba[:,:,:3] = out_frame
         out_frame_rgba[:,:,3] = 255
-        print(out_frame_rgba.shape)
+        print(out_frame_rgba)
         cam.send(frame)
         cam.sleep_until_next_frame()
