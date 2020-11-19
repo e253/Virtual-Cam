b9825f88662b7d3e08989b3a19eab0b72fcd6b30
e253 <62357808+e253@users.noreply.github.com>
2020-11-18 21:44:38 -0500

    Broken but commiting anyway

--- Test.py
+++ Test.py
@@ -1,7 +1,25 @@
 import pyvirtualcam
 import cv2
+import numpy as np
 
 
+video  = cv2.VideoCapture("C:\\Users\\Ethan\\Videos\\Transformers\\Transformers (2009 ) Revenge Of The Fallen.mp4")
+webcam = cv2.VideoCapture(0)
+INPUT = webcam
+width = int(INPUT.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(INPUT.get(cv2.CAP_PROP_FRAME_HEIGHT))
+fps_in = INPUT.get(cv2.CAP_PROP_FPS)
 
-with pyvirtualcam.Camera(1280, 720, 30, 0, print_fps=True) as cam:
-    while True
+
+with pyvirtualcam.Camera(width, height, fps_in, 0) as cam:
+    while True:
+        status, frame = video.read()
+        print(frame.shape)
+        out_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        print(out_frame.shape)
+        out_frame_rgba = np.zeros((height, width, 4), np.uint8)
+        out_frame_rgba[:,:,:3] = out_frame
+        out_frame_rgba[:,:,3] = 255
+        print(out_frame_rgba.shape)
+        cam.send(frame)
+        cam.sleep_until_next_frame()
