6a3e5ce5dd348518e6702c98bffa9c71a345afe4
e253 <62357808+e253@users.noreply.github.com>
2020-11-21 11:40:51 -0500

    Last Commit - Still Doesn't work

--- Test.py
+++ Test.py
@@ -11,15 +11,16 @@ height = int(INPUT.get(cv2.CAP_PROP_FRAME_HEIGHT))
 fps_in = int(INPUT.get(cv2.CAP_PROP_FPS))
 print(height, width, fps_in)
 
-with pyvirtualcam.Camera(width, height, fps_in, 0) as cam:
+with pyvirtualcam.Camera(width, height, fps_in, print_fps=True) as cam:
     while True:
+        # Get Input Frame
         status, frame = INPUT.read()
-        print(frame)
         out_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        print(out_frame)
         out_frame_rgba = np.zeros((height, width, 4), np.uint8)
         out_frame_rgba[:,:,:3] = out_frame
         out_frame_rgba[:,:,3] = 255
-        print(out_frame_rgba)
+        out_frame_rgba.reshape(height, width, 4)
+        assert(out_frame_rgba.shape[2] == 4)
+        #Send Output Frame
         cam.send(frame)
         cam.sleep_until_next_frame()
